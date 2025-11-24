16fc3743426b26696fdc02c34d10787a58708df5
Ty Dunn <ty@continue.dev>
2025-11-24 14:03:15 -0800

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Continuous AI",
   description: "The practice of developing software with background agents",
+  icons: {
+    icon: '/favicon.svg?v=2',
+  },
 };
 
 export default function RootLayout({
@@ -14,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.svg?v=2" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
